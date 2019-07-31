19bb44ea4a4eca55a940b2f43b1a961bbc9d2a0f
Kaka Ruto <borecollins@gmail.com>
2019-07-31 15:17:28 +0300

    Show navigation buttons

--- spec/features/answer_questions_spec.rb
+++ spec/features/answer_questions_spec.rb
@@ -16,8 +16,21 @@ RSpec.feature 'Answering questions', :type => :feature do
   scenario 'User clicks Take Questions button' do
     visit '/'
 
-    click_button('Take Questions')
+    click_button 'Take Questions'
 
     expect(page).to have_text('Are you an investor?')
+    expect(page).to have_selector(:link_or_button, 'Next')
+    expect(page).not_to have_selector(:link_or_button, 'Previous')
+  end
+
+  scenario 'User clicks Next button' do
+    visit '/'
+
+    click_button 'Take Questions'
+    click_button 'Next'
+
+    expect(page).to have_text 'How long have you invested?'
+    expect(page).to have_selector(:link_or_button, 'Previous')
+    expect(page).not_to have_selector(:link_or_button, 'Next')
   end
 end
