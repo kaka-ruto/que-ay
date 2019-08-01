9df0380e7a88ded0b394b9f15afde447e4b5ea93
Kaka Ruto <borecollins@gmail.com>
2019-08-01 03:03:56 +0300

    Fix bug with the previous button

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
-    @previous = Question.where('id < ?', params[:id]).order(:id).first
+    @previous = Question.where('id < ?', params[:id]).order(:id).last
     @next = Question.where('id > ?', params[:id]).order(:id).first
   end
 end
