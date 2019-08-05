5b4b1277ff306dc6c5190b23ff98f4de2ee3a88a
Kaka Ruto <borecollins@gmail.com>
2019-08-05 03:55:41 +0300

    Show test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'support/setup/factory_bot'
 require 'support/setup/shoulda_matchers'
+require 'support/setup/simplecov'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
---
+++ spec/support/setup/simplecov.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/channels/'
+  add_filter '/mailers/'
+  add_filter '/jobs/'
+  add_filter '/helpers/'
+  add_filter '/libraries/'
+end
